8b394f644b2e04aeffc0318d4ce8233764caebf7
Reto Stauffer <reto.stauffer@uibk.ac.at>
2021-12-21 08:17:55 +0100

    Working on django app

--- s3.py
+++ s3.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     s3URL = "https://s3.console.aws.amazon.com/s3/buckets/euppparquet-analysis"
     bucket = "s3://euppparquet-analysis"
     filters = [("time", "=", 12), ("time", "=", "6"), ("day", "=", 1)]
+    filters = [("time", "=", 12), ("time", "=", "6"), ("day", "=", 1), ("param", "=", "mn2t")]
     data = parquet.ParquetDataset(bucket, filesystem = s3,
             use_legacy_dataset = True, filters = filters).read_pandas().to_pandas()
 
